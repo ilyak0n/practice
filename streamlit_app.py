3d0210901479d159321efa00c00bd0e9af284102
ilyak0n <144427269+ilyak0n@users.noreply.github.com>
2025-07-24 12:44:34 +0300

    Update streamlit_app.py
    delete some import

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import altair as alt
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.express as px
 
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.model_selection import train_test_split
